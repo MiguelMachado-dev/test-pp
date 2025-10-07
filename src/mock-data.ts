e5cfcbad274dbfd82113593677032967df6e2832
Miguel Machado <hello@miguelmachado.dev>
2025-10-07 19:21:46 -0300

    chore(mock): in-memory data for testing

---
+++ src/mock-data.ts
@@ -0,0 +1,26 @@
+export const Users = [
+  {
+    id: "test",
+    name: "John Doe",
+    email: "john@doe.com",
+    age: 23,
+  },
+  {
+    id: "test-1",
+    name: "User A",
+    email: "john@doe.com",
+    age: 64,
+  },
+  {
+    id: "test-2",
+    name: "User B",
+    email: "john@doe.com",
+    age: 28,
+  },
+  {
+    id: "test-3",
+    name: "User C",
+    email: "john@doe.com",
+    age: 78,
+  },
+];
